4935f2f085389727fe307621196af7903d3de550
Taylor Kelley <taylorgkelley04@gmail.com>
2026-01-22 13:13:25 -0600

    feat: added basic better-auth client to nextjs web application with some styling for the home page as a sample usage

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,6 +1,13 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  rewrites: () => [
+    // rewrites requests to /api and forwards it to /api/* for better-auth integration
+    {
+      source: '/api/:path*',
+      destination: `${process.env.API_URL}/api/:path*`,
+    },
+  ],
   transpilePackages: ['@repo/ui', 'react-native-web'],
 };
 
