3e4eedaca5122aaf91b5aeecc8a88707d67ec8d7
Taylor Kelley <tkelley@LGHIP.ORG>
2026-03-03 12:47:56 -0600

    feat: adding cursor utility class for easier decoding/encoding

---
+++ apps/api/src/utils/cursor.util.ts
@@ -0,0 +1,12 @@
+export default class Cursor {
+  public static encode(data: any): string {
+    return Buffer.from(JSON.stringify(data)).toString('base64');
+  }
+
+  public static decode(data: string): Record<string, any> {
+    return JSON.parse(Buffer.from(data, 'base64').toString('utf-8')) as Record<
+      string,
+      any
+    >;
+  }
+}
